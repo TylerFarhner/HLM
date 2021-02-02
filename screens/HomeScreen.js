3719a604441b80c54d4096139ee76c39605cfcd9
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-02 15:05:36 -0800

    fetches token on homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function HomeScreen() {
+
+    const loadProfile = async () => {
+        const token = await AsyncStorage.getItem('token')
+        console.log(token)
+    }
+    
+    useEffect(() => {
+        loadProfile()
+    })
+
     return (
         <View>
             <Text>Home Screen</Text>
