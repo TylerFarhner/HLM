410d33b60efb410ce09387cfc543547976509c54
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-02 15:08:19 -0800

    adds redirect to login if no token

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function HomeScreen() {
+export default function HomeScreen(props) {
 
     const loadProfile = async () => {
         const token = await AsyncStorage.getItem('token')
+        if(!token) {
+            props.navigation.navigate('Login')
+        }
         console.log(token)
     }
     
