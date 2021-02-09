f6a2158ba854fef61737c60f107212e081128528
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-09 09:00:48 -0800

    adds profile screen

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function ProfileScreen() {
+    return (
+        <View>
+            <Text>Profile Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+    text: {
+        fontSize: 20
+    }
+
+})
