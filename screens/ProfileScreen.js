a582835825c57abc53a799d3caa7646846458287
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-10 00:11:13 -0800

    adds profile screen, moves logout

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,66 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function ProfileScreen() {
+const jwtDecode = require('jwt-decode')
+
+export default function ProfileScreen(props) {
+
+    const [ fullName, setFullName ] = useState('')
+    const [ email, setEmail ] = useState('')
+
+    const loadProfile = async () => {
+        const token = await AsyncStorage.getItem('token')
+        if(!token) {
+            props.navigation.navigate('Login')
+        }
+
+        const decoded = jwtDecode(token)
+        setFullName(decoded.fullName)
+        setEmail(decoded.email)
+    }
+    
+    const logout = props => {
+        AsyncStorage.removeItem('token')
+            .then(() => {
+                props.navigation.navigate('Login')
+            })
+            .catch(err => console.log(err))
+    }
+    
+    useEffect(() => {
+        loadProfile()
+    }, [])
+    
     return (
-        <View>
-            <Text>Profile Screen</Text>
+        <View style={ styles.container }>
+            <View>
+                <Text style={ styles.text }>Welcome { fullName ? fullName: '' }</Text>
+            </View>
+            <View>
+                <Button 
+                    title="Logout"
+                    onPress={() => logout(props)}
+                />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
-    text: {
-        fontSize: 20
-    }
+    container: {
+        flex: 1,
+        // padding: 40
+    },
 
+    text: {
+        fontSize: 22
+    },
+
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+    
 })
